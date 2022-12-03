2f7310a5d6c1044ec07dc88fc226e5ffa1ea2558
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2022-12-03 03:28:54 +0100

    Create page components with dummy content

---
+++ src/components/Projects.js
@@ -0,0 +1,31 @@
+import React, { Fragment } from 'react';
+
+export default function Projects() {
+  return (
+    <Fragment>
+      <h1>Projects</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+        eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
+        voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
+        clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
+        amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+        nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+        sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
+        rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
+        ipsum dolor sit amet.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+        eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
+        voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
+        clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
+        amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+        nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+        sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
+        rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
+        ipsum dolor sit amet.
+      </p>
+    </Fragment>
+  );
+}
--- src/components/Router.js
+++ src/components/Router.js
@@ -1,15 +1,22 @@
 import React, { lazy } from 'react';
 import { Switch, Route } from 'react-router-dom';
 
+// page components
+const About = lazy(() => import('./About'));
+const Contact = lazy(() => import('./Contact'));
 const Home = lazy(() => import('./Home'));
 const LegalNotice = lazy(() => import('./LegalNotice'));
 const NotFound = lazy(() => import('./NotFound'));
 const PrivacyPolicy = lazy(() => import('./PrivacyPolicy'));
+const Projects = lazy(() => import('./Projects'));
 
 export default function Router() {
   return (
     <Switch>
       <Route exact path="/" component={Home} />
+      <Route exact path="/about" component={About} />
+      <Route exact path="/projects" component={Projects} />
+      <Route exact path="/contact" component={Contact} />
       <Route exact path="/legal-notice" component={LegalNotice} />
       <Route exact path="/privacy-policy" component={PrivacyPolicy} />
       <Route component={NotFound} />
